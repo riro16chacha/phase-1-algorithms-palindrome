cad1214a8e4ac5b8e30c354c05cbf95c9fb3e874
Riro Maclaurine Chacha <riro.chacha@student.moringaschool.com>
2023-04-18 20:48:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  
+    // Convert the string to lowercase and remove non-alphanumeric characters
+    word = word.toLowerCase().replace(/[^a-z0-9]/g, '');
+  
+    // Check if the string reads the same forwards and backwards
+    return word === word.split('').reverse().join('');
+  
 }
 
 /* 
@@ -8,6 +15,7 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
+  The function takes a string as an argument, converts it to lowercase, removes non-alphanumeric characters using a regular expression, and then checks if the resulting string is readable both frontwards and backwards.
 */
 
 // You can run `node index.js` to view these console logs
